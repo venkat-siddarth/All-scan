634e1ef60964e50046ec3c793c27255a5cce2c88
V Venkat Siddarth Reddy <60511416+venkat-siddarth@users.noreply.github.com>
2022-08-24 21:09:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,12 @@ const app=require("./app.js");
 const dotenv=require('dotenv');
 const mongoose=require("mongoose");
 dotenv.config({path:'.env'});
+var port = process.env.PORT || 8080;
 mongoose.connect("mongodb+srv://SidduTarp:R4w1AKYOGTw5sAze@cluster0.ooihiva.mongodb.net/?retryWrites=true&w=majority",
 { useNewUrlParser: true, useUnifiedTopology: true }, err => {
         console.log('connected')
 });
-app.listen(8080,()=>{
+app.listen(port,()=>{
     console.log(`Back-end running at 8080`);
 })
 
